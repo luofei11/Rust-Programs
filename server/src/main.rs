ad70fe807debfd9f88311f96b14856aa3d26af68
Fei(Flammy) Luo <luofeithu@foxmail.com>
2016-02-18 16:28:40 -0600

    parse file

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,21 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::io::BufReader;
+use std::path::Path;
+use std::io::ErrorKind;
+use std::fs::File;
 extern crate time;
 
 struct Request{
     request_file: String,
+    request_time: String,
     stream: TcpStream,
 }
 
 impl Request{
-    fn new(&mut self, stream: TcpStream) -> Self{
+    fn new(&mut self, stream: TcpStream, time: String) -> Self{
         // get path of requested file
+        self.request_time = time;
         let mut request_header = Strng::new();
         let mut stream_reader = BufReader::new(stream);
         let mut request_parameters = Vec<&str>::new();
@@ -45,7 +50,15 @@ impl Handler {
     }
 
     fn parse_file(&mut self){
-        unimplemented!()
+        let path = Path::new(&self.request.request_file);
+        let mut f = try!(File::open(path));
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Ok(_) => self.set_response(200, s),
+            Err(e) => {
+                //match e.kind()
+            },
+        }
     }
 
     fn parse_dir(&mut self) {
@@ -58,12 +71,12 @@ impl Handler {
 struct Response {
     status_code: usize,
     status_info: String,
-
+    file_content: Option<String>,
 }
 fn handle_client(stream: TcpStream){
     // Record current time for log file
     let current_time = time::now().ctime().to_string();
-    let mut req = Request::new(stream);
+    let mut req = Request::new(stream,current_time);
     let mut handler = Handler::new(req);
     let mut res = handler.get_response();
 
